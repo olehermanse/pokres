ec9d809ffa1a4d028c5f90e5844a4cabbdb4d36e
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-11-05 22:13:52 -0800

    Inline JSON and Drawer Navigation

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import StatCalculator from './StatCalculator';
+
+import g1 from '../web/files/gen1.mini.json';
+import g2 from '../web/files/gen2-5.mini.json';
+import g6 from '../web/files/gen6.mini.json';
+import g7 from '../web/files/gen7.mini.json';
+
+export default class PokresApp extends React.Component {
+  state = {data: g1,
+           open: false,
+           title: "Generation 1"};
+
+   handleToggle = () => this.setState({open: !this.state.open});
+   handleClose = () => this.setState({open: false});
+
+  gen1 = () => {
+    this.setState({open: false, title: "Generation 1", data: g1});
+  }
+
+  gen25 = () => {
+    this.setState({open: false, title: "Generation 2-5", data: g2});
+  }
+
+  gen6 = () => {
+    this.setState({open: false, title: "Generation 6", data: g6});
+  }
+  gen7 = () => {
+    this.setState({open: false, title: "Generation 7", data: g7});
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="nav_bar">
+          <AppBar title={this.state.title}
+          onLeftIconButtonTouchTap={this.handleToggle}
+          />
+        </div>
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem onClick={this.gen1}>Generation 1</MenuItem>
+          <MenuItem onClick={this.gen25}>Generation 2-5</MenuItem>
+          <MenuItem onClick={this.gen6}>Generation 6</MenuItem>
+          <MenuItem onClick={this.gen7}>Generation 7</MenuItem>
+          <MenuItem onClick={this.gen1}>Information</MenuItem>
+        </Drawer>
+
+        <StatCalculator data={this.state.data}/>
+      </div>
+    );
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import StatCalculator from './StatCalculator';
-import AppBar from 'material-ui/AppBar';
+import PokresApp from './App';
 
-function loadJSON(path, callback) {
-    var xhr = new XMLHttpRequest;
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                callback(JSON.parse(xhr.responseText));
-            } else {
-              console.log("Error during GET");
-            }
-        }
-    };
-    xhr.open("GET", path, true);
-    xhr.send();
-    return xhr.onreadystatechange();
-}
+const App = () => (
+  <MuiThemeProvider>
+    <PokresApp />
+  </MuiThemeProvider>
+);
 
-function renderUI(getData)
-{
-  const App = () => (
-    <MuiThemeProvider>
-      <div>
-        <div className="nav_bar">
-          <AppBar title="PokRes"/>
-        </div>
-        <StatCalculator data={getData}/>
-      </div>
-    </MuiThemeProvider>
-  );
-
-  ReactDOM.render(
-    <App />,
-    document.getElementById('body_main')
-  );
-}
+ReactDOM.render(
+  <App />,
+  document.getElementById('body_main')
+);
 
 document.body.style.overflow = "hidden";
-renderUI(false);
-
-(function data_init(){
-  loadJSON("https://olehermanse.github.io/pokres/web/files/gen1.json",
-           renderUI);
-})();
